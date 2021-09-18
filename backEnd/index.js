5fefe0d99de8377f56c925661ce722ba10a6b8c0
pawankundar <pawankundar98@hotmail.com>
2021-09-18 11:11:47 +0530

    mongoose connect catch function added

--- backEnd/index.js
+++ backEnd/index.js
@@ -5,21 +5,21 @@ const helmet = require("helmet");
 const morgan = require("morgan");
 require("dotenv").config();
 const userRoute = require("./router/users");
-const authRoute = require('./router/auth')
+const authRoute = require("./router/auth");
 
 App.use(express.json());
 App.use(helmet());
 App.use(morgan("common"));
-App.use("/api/users",userRoute);
-App.use("/api/auth",authRoute)
+App.use("/api/users", userRoute);
+App.use("/api/auth", authRoute);
 
-mongoose.connect(
-  process.env.MONGO_URL,
-  { useNewUrlParser: true, useUnifiedTopology: true },
-  () => {
-    console.log("database connected");
-  }
-);
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(console.log("database connected"))
+  .catch((err) => console.log("error is ",err));
 
 App.listen(process.env.PORT, () => {
   console.log("server running");
