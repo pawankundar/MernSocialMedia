f8bfd69ae391fa9e9dca6eb9cb79fc72aeaa24a9
pawankundar <pawankundar98@hotmail.com>
2021-09-22 20:14:54 +0530

    Navigation bar component

---
+++ frontend/src/components/navBar/navBar.js
@@ -0,0 +1,41 @@
+import { Chat, Notifications, Person, Search } from "@material-ui/icons";
+import "./navBar.css";
+
+const NavBar = () => {
+  return (
+    <div className="navBarContainer">
+      <div className="navBarLeft">
+        <span className="logo">Dont FaceBook</span>
+      </div>
+      <div className="navBarCenter">
+        <div className="searchBar">
+          <Search  className = "searchIcon"/>
+          <input type="text" className="searchInput" placeholder="Search" />
+        </div>
+      </div>
+      <div className="navBarRight">
+        <div className="navBarLinks">
+          <span className="navBarLink">Homepage</span>
+          <span className="navBarLink">Timeline</span>
+        </div>
+        <div className="navBarIcons">
+          <div className="navBarIconItem">
+            <Person />
+            <span className="iconBadge">1</span>
+          </div>
+          <div className="navBarIconItem">
+            <Chat />
+            <span className="iconBadge">1</span>
+          </div>
+          <div className="navBarIconItem">
+            <Notifications />
+            <span className="iconBadge">1</span>
+          </div>
+        </div>
+        <img classname = "profilePic" src="/assets/person/1.jpeg" alt ="profilepicture"  className="profilePic"/>
+      </div>
+    </div>
+  );
+};
+
+export default NavBar;
---
+++ frontend/src/pages/home/home.js
@@ -0,0 +1,12 @@
+import NavBar from "../../components/navBar/navBar";
+
+const Home = () => {
+  return(
+      <>
+       <NavBar/>
+       <div>Home Page</div>
+      </>
+  );
+};
+
+export default Home;
