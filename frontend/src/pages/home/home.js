78408e8ca1e5db3f4578540d4d66a46f7cbcb09c
pawankundar <pawankundar98@hotmail.com>
2021-09-23 00:36:25 +0530

    HomePage

--- frontend/src/pages/home/home.js
+++ frontend/src/pages/home/home.js
@@ -1,11 +1,19 @@
 import NavBar from "../../components/navBar/navBar";
+import Feed from "../../components/feed/Feed";
+import RightBar from "../../components/rightBar/RightBar";
+import LeftBar from "../../components/leftBar/LeftBar";
+import "./home.css";
 
 const Home = () => {
-  return(
-      <>
-       <NavBar/>
-       <div>Home Page</div>
-      </>
+  return (
+    <>
+      <NavBar />
+      <div className="Container">
+        <LeftBar />
+        <Feed />
+        <RightBar />
+      </div>
+    </>
   );
 };
 
