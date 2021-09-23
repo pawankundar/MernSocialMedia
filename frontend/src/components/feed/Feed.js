bb36faf2ec1a2b5e3bb4bd991c9713b5e0101d8a
pawankundar <pawankundar98@hotmail.com>
2021-09-23 13:22:32 +0530

    Feed component

---
+++ frontend/src/components/feed/Feed.js
@@ -0,0 +1,18 @@
+import Post from "../post/Post";
+import Share from "../share/Share";
+import "./Feed.css";
+
+const Feed = () => {
+  return (
+    <div className="feed">
+      <div className="feedWrapper">
+           <Share/>
+           <Post/>
+           <Post/>
+           <Post/>
+      </div>
+    </div>
+  );
+};
+
+export default Feed;
