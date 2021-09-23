ac3c388973279189fd4cfe250e7d4eca0bc3a6a4
pawankundar <pawankundar98@hotmail.com>
2021-09-24 00:44:48 +0530

    Login page component

---
+++ frontend/src/pages/login/Login.js
@@ -0,0 +1,27 @@
+import "./Login.css"
+
+const Login = ()=>{
+    return(
+        <div className="login">
+            <div className="loginWrapper">
+                <div className="loginLeft">
+                    <h3 className="loginLogo">Panchayat 🏡</h3>
+                    <span className="loginDesc">
+                        Connect with friends around the world on Panchayat.
+                    </span>
+                </div>
+                <div className="loginRight">
+                    <div className="loginBox">
+                        <input  className="loginInput" placeholder="Email"/>
+                        <input  className="loginInput" placeholder="Password"/>
+                        <button className="loginButton">Login</button>
+                        <span className="loginForgort">Forgort Password ?</span>
+                        <button className="registerButton">Create a new account</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Login
