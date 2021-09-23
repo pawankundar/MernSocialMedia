479cecfd17b9e0b23020dbae9fc9bd4614adbad1
pawankundar <pawankundar98@hotmail.com>
2021-09-23 17:14:13 +0530

    mapping post from data

--- frontend/src/components/feed/Feed.js
+++ frontend/src/components/feed/Feed.js
@@ -1,15 +1,17 @@
 import Post from "../post/Post";
 import Share from "../share/Share";
 import "./Feed.css";
+import {Posts } from "../../dummyData"
 
 const Feed = () => {
   return (
     <div className="feed">
       <div className="feedWrapper">
            <Share/>
-           <Post/>
-           <Post/>
-           <Post/>
+           {Posts.map((data)=>(
+            <Post data={data} key={data.id}/>
+           ))}
+           
       </div>
     </div>
   );
