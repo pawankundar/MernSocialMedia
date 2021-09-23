c90ae49e612f80970cb0a28bd59032d03ce2e260
pawankundar <pawankundar98@hotmail.com>
2021-09-23 17:17:21 +0530

    data from json

--- frontend/src/components/post/Post.js
+++ frontend/src/components/post/Post.js
@@ -1,31 +1,39 @@
 import { MoreVert } from "@material-ui/icons"
 import "./Post.css"
-const Post = ()=>{
+import {Users } from "../../dummyData"
+import { useState } from "react"
+const Post = ({data})=>{
+    const[like,setLike] =useState(data.like)
+    const[isLiked,setIsLiked] = useState(false)
+    const LikeUpdater = ()=>{
+        setLike(isLiked?like-1:like+1)
+        setIsLiked(!isLiked)
+    }
     return(
         <div className="post">
             <div className="postWrapper">
                 <div className="postTop">
                     <div className="postLeft">
-                        <img className="postProfilePic" src="/assets/person/1.jpeg" alt="ProfilePic"/>
-                        <span className="postUsername">Jane Doe</span>
-                        <span className="postTime">5 mins ago.</span>
+                        <img className="postProfilePic" src={Users[data.id-1].profilePicture} alt="ProfilePic"/>
+                        <span className="postUsername">{Users[data.id-1].username}</span>
+                        <span className="postTime">{data.date}</span>
                     </div>
                     <div className="postRight">
                         <MoreVert className="postMoreIcon"/>
                     </div>
                 </div>
                 <div className="postCenter">
-                    <span className="postCaption">Hello world</span>
-                    <img className="postImage" src="/assets/post/1.jpeg" alt="PostedImage"/> 
+                    <span className="postCaption">{data.desc}</span>
+                    <img className="postImage" src={data.photo} alt="PostedImage"/> 
                 </div>
                 <div className="postBottom">
                     <div className="postBottomLeft">
-                        <img className="likeIcon" alt ="" src="/assets/like.png"/>
+                        <img className="likeIcon" onClick={LikeUpdater} alt ="" src="/assets/like.png"/>
                         <img className="likeIcon" alt ="" src="/assets/heart.png"/>
-                        <span className="likeCount">11 People liked this.</span>
+                        <span className="likeCount">{like} People liked this.</span>
                     </div>
                     <div className="postBottomRight">
-                        <span className="postComment">11 comments.</span>
+                        <span className="postComment">{data.comment} comments.</span>
                     </div>
                 </div>
             </div>
