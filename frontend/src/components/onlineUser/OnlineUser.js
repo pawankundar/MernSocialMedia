c68fdb242584a269ce487720858e73bece12eba5
pawankundar <pawankundar98@hotmail.com>
2021-09-25 01:55:41 +0530

    added image link

--- frontend/src/components/onlineUser/OnlineUser.js
+++ frontend/src/components/onlineUser/OnlineUser.js
@@ -1,9 +1,10 @@
 import "./OnlineUser.css"
 const OnlineUser = ({data})=>{
+    const photoUrl = process.env.REACT_APP_PUBLIC_FOLDER
     return(
         <li className="friendOnline">
         <div className="profileImageContainer">
-            <img className="profileImage" alt="OnlineFriend" src={data.profilePicture}/>
+            <img className="profileImage" alt="OnlineFriend" src={photoUrl+data.profilePicture}/>
             <span className="profileOnlineBadge"></span>
         </div>
         <span className="profileName">{data.username}</span> 
