c1deaafc2ae2582aa6fbeebc519c11d3e8b41354
pawankundar <pawankundar98@hotmail.com>
2021-09-25 01:57:21 +0530

    React router path added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,4 @@
+import { BrowserRouter, Switch, Route} from "react-router-dom";
 import Home from "./pages/home/home";
 import Login from "./pages/login/Login";
 import ProfilePage from "./pages/profilePage/ProfilePage";
@@ -5,11 +6,15 @@ import Register from "./pages/register/Register";
 
 const App = () => {
   return (
-  <Home/>
-  // <ProfilePage/>
-  // <Login/>
-  // <Register/>
-  )
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/profile/:username" component={ProfilePage} />
+        <Route path="/login" exact component={Login} />
+        <Route path="/Register" exact component={Register} />
+      </Switch>
+    </BrowserRouter>
+  );
 };
 
 export default App;
