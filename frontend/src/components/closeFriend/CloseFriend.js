5f321eb7d46fc97d75ef0eea1264b5af6b521f4e
pawankundar <pawankundar98@hotmail.com>
2021-09-25 01:57:05 +0530

    Photo url added

--- frontend/src/components/closeFriend/CloseFriend.js
+++ frontend/src/components/closeFriend/CloseFriend.js
@@ -1,11 +1,18 @@
-import "./CloseFriend.css"
-const CloseFriend = ({data})=>{
-    return (
-        <li className="leftBarFriend">
-        <img className="leftBarFriendPic" src={data.profilePicture} alt="profilePic" />
-        <span className="leftBarFriendName">{data.username}</span>
-      </li>
-    )
-}
 
-export default CloseFriend
+import "./CloseFriend.css";
+
+const CloseFriend = ({ data }) => {
+  const photoUrl = process.env.REACT_APP_PUBLIC_FOLDER;
+  return (
+    <li className="leftBarFriend">
+      <img
+        className="leftBarFriendPic"
+        src={photoUrl + data.profilePicture}
+        alt="profilePic"
+      />
+      <span className="leftBarFriendName">{data.username}</span>
+    </li>
+  );
+};
+
+export default CloseFriend;
