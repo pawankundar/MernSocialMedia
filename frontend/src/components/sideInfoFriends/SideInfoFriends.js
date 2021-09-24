37a51f5ef35633c28b63e2a164042d40b6788c67
pawankundar <pawankundar98@hotmail.com>
2021-09-25 01:54:46 +0530

    photoUrl added

--- frontend/src/components/sideInfoFriends/SideInfoFriends.js
+++ frontend/src/components/sideInfoFriends/SideInfoFriends.js
@@ -1,8 +1,10 @@
 import "./SideInfoFriends.css"
 const SideInfoFriends = ({name,profilePic}) => {
+  const photoUrl = process.env.REACT_APP_PUBLIC_FOLDER
+
   return (
     <div className="rightBarFollowing">
-    <img className="rightBarFollowingImage" src={`/assets/person/${profilePic}.jpeg`} alt="followingPic"/>
+    <img className="rightBarFollowingImage" src={`${photoUrl}person/${profilePic}.jpeg`} alt="followingPic"/>
     <span className="rightBarFollowingName">{name}</span>
 </div>
   );
