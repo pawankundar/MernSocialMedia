773f70a435205ab60341ca24f98f87ed939faf22
pawankundar <pawankundar98@hotmail.com>
2021-09-26 00:00:10 +0530

    added context API

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,5 +1,11 @@
 import React from "react";
 import reactDom from "react-dom";
-import App from "./App"
+import App from "./App";
+import { ContextProvider } from "./context/context";
 
-reactDom.render(<App/>,document.getElementById("root"))
+reactDom.render(
+  <ContextProvider>
+    <App />
+  </ContextProvider>,
+  document.getElementById("root")
+);
