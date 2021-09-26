6ec682dabb4ff2412b613358c34aa21aabfec17d
pawankundar <pawankundar98@hotmail.com>
2021-09-26 20:17:33 +0530

    added logut and displaying userprofile pic from context

--- frontend/src/components/navBar/navBar.js
+++ frontend/src/components/navBar/navBar.js
@@ -1,25 +1,36 @@
 import { Chat, Notifications, Person, Search } from "@material-ui/icons";
+import { useContext } from "react";
 import { Link } from "react-router-dom";
+import { Context } from "../../context/context";
 import "./navBar.css";
 
 const NavBar = () => {
+
+  const ImageLink = process.env.REACT_APP_PUBLIC_FOLDER;
+  const { dispatch, user } = useContext(Context);
+
+  const handleLogout = () => {
+    dispatch({ type: "LOGOUT" });
+  };
   return (
     <div className="navBarContainer">
       <div className="navBarLeft">
-        <Link to="/" style={{ textDecoration: 'none' }}>
-        <span className="logo">Panchayat 🏡</span>
+        <Link to="/" style={{ textDecoration: "none" }}>
+          <span className="logo">Panchayat 🏡</span>
         </Link>
       </div>
       <div className="navBarCenter">
         <div className="searchBar">
-          <Search  className = "searchIcon"/>
+          <Search className="searchIcon" />
           <input type="text" className="searchInput" placeholder="Search" />
         </div>
       </div>
       <div className="navBarRight">
         <div className="navBarLinks">
           <span className="navBarLink">Homepage</span>
-          <span className="navBarLink">Timeline</span>
+          <span className="navBarLink" onClick={handleLogout}>
+            Logout
+          </span>
         </div>
         <div className="navBarIcons">
           <div className="navBarIconItem">
@@ -35,8 +46,13 @@ const NavBar = () => {
             <span className="iconBadge">1</span>
           </div>
         </div>
-  
-        <img classname = "profilePic" src="/assets/person/1.jpeg" alt ="profilepicture"  className="profilePic"/>
+
+        <img
+          classname="profilePic"
+          src={user.profilePicture?ImageLink+user.profilePicture:ImageLink+"/person/no-avatar.png"}
+          alt="profilepicture"
+          className="profilePic"
+        />
       </div>
     </div>
   );
