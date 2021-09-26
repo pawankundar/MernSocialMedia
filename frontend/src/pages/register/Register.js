0f20fbedcc548298ca34da9cb3513914bacfebaa
pawankundar <pawankundar98@hotmail.com>
2021-09-26 20:20:05 +0530

    connected with register user Api

--- frontend/src/pages/register/Register.js
+++ frontend/src/pages/register/Register.js
@@ -1,6 +1,46 @@
+import { Box, Modal, Typography } from "@material-ui/core";
+import axios from "axios";
+import { useRef, useState } from "react";
+import { Link } from "react-router-dom";
 import "./Register.css";
 
 const Register = () => {
+  const usernameRef = useRef();
+  const emailRef = useRef();
+  const passwordRef = useRef();
+  const confirmPassword = useRef();
+
+  const [error, setError] = useState(false);
+  const [success, setSuccess] = useState(false);
+  const handleClose = () => setError(false);
+  const handleSignUp = async (e) => {
+    e.preventDefault();
+    await axios
+      .post("auth/register", {
+        username: usernameRef.current.value,
+        email: emailRef.current.value,
+        password: passwordRef.current.value,
+      })
+      .then(() => setSuccess(true))
+      .catch(() => setError(true));
+  };
+  const style = {
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    width: 400,
+    bgcolor: "background.paper",
+    border: "2px solid #000",
+    boxShadow: 24,
+    borderRadius: 30,
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    p: 4,
+  };
+  setTimeout(() => success && window.location.replace("/login"), 3000);
   return (
     <div className="register">
       <div className="registerWrapper">
@@ -11,14 +51,69 @@ const Register = () => {
           </span>
         </div>
         <div className="registerRight">
-          <div className="registerBox">
-            <input className="registerInput" placeholder="Username" />
-            <input className="registerInput" placeholder="Email" />
-            <input className="registerInput" placeholder="Password" />
-            <input className="registerInput" placeholder="Confirm Password" />
+          <form className="registerBox" onSubmit={handleSignUp}>
+            <input
+              className="registerInput"
+              placeholder="Username"
+              ref={usernameRef}
+              required
+            />
+            <input
+              className="registerInput"
+              placeholder="Email"
+              type="email"
+              ref={emailRef}
+              required
+            />
+            <input
+              className="registerInput"
+              placeholder="Password"
+              type="password"
+              ref={passwordRef}
+              required
+            />
+            <input
+              className="registerInput"
+              placeholder="Confirm Password"
+              type="password"
+              ref={confirmPassword}
+              required
+            />
             <button className="registerButton">Register</button>
+            <Link to="/login">
             <button className="newLoginButton">Login</button>
-          </div>
+            </Link>
+          </form>
+          <Modal
+            open={error}
+            onClose={handleClose}
+            aria-labelledby="modal-modal-title"
+            aria-describedby="modal-modal-description"
+          >
+            <Box sx={style}>
+              <Typography id="modal-modal-title" variant="h6" component="h2">
+                Error in registration
+              </Typography>
+              <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+                Please check your credentails and try again
+              </Typography>
+            </Box>
+          </Modal>
+          <Modal
+            open={success}
+            onClose={handleClose}
+            aria-labelledby="modal-modal-title"
+            aria-describedby="modal-modal-description"
+          >
+            <Box sx={style}>
+              <Typography id="modal-modal-title" variant="h6" component="h2">
+                User registered
+              </Typography>
+              <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+                Redirecting to login page ..
+              </Typography>
+            </Box>
+          </Modal>
         </div>
       </div>
     </div>
