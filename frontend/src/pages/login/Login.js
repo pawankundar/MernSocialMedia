fe293a51949e7be96343ec23d434b61219444b36
pawankundar <pawankundar98@hotmail.com>
2021-09-26 20:20:44 +0530

    connected with login user Api and added dispatch

--- frontend/src/pages/login/Login.js
+++ frontend/src/pages/login/Login.js
@@ -3,14 +3,15 @@ import { useContext, useRef } from "react";
 import axios from "axios";
 import { Context } from "../../context/context";
 import { CircularProgress } from "@material-ui/core";
+import { Link } from "react-router-dom";
 
 const Login = () => {
   const username = useRef();
   const password = useRef();
-  const { dispatch , isFetching,user} = useContext(Context);
+  const { dispatch, isFetching, user } = useContext(Context);
   const handleSubmit = async (e) => {
     e.preventDefault();
-    dispatch({type : "LOGIN_START"})
+    dispatch({ type: "LOGIN_START" });
     await axios
       .post("auth/login", {
         username: username.current.value,
@@ -19,7 +20,7 @@ const Login = () => {
       .then((resp) => dispatch({ type: "LOGIN_SUCCESS", payload: resp.data }))
       .catch((err) => dispatch({ type: "LOGIN_FAILURE", payload: err }));
   };
-  console.log(user)
+  console.log(user);
 
   return (
     <div className="login">
@@ -35,20 +36,29 @@ const Login = () => {
             <input
               className="loginInput"
               placeholder="User name"
-              
               ref={username}
+              required
             />
             <input
               className="loginInput"
               placeholder="Password"
               type="password"
               required
-              
               ref={password}
             />
-            <button className="loginButton">{isFetching?<CircularProgress color="white" size="15px"/>:"Login"}</button>
+            <button className="loginButton">
+              {isFetching ? (
+                <CircularProgress color="white" size="15px" />
+              ) : (
+                "Login"
+              )}
+            </button>
             <span className="loginForgort">Forgort Password ?</span>
-            <button className="registerXButton">{isFetching?<CircularProgress color="white" size="15px"/>:"Create a new account"}</button>
+            <Link  to="/register" >
+              <button className="registerXButton" type="button">
+                create a new account
+              </button>
+            </Link>
           </form>
         </div>
       </div>
