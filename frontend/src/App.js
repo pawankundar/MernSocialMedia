f88a47a06732b77dfd820fefef8b4136ea0b4b20
pawankundar <pawankundar98@hotmail.com>
2021-09-26 20:13:39 +0530

    added user from context

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,20 @@
+import { useContext } from "react";
 import { BrowserRouter, Switch, Route} from "react-router-dom";
+import { Context } from "./context/context";
 import Home from "./pages/home/home";
 import Login from "./pages/login/Login";
 import ProfilePage from "./pages/profilePage/ProfilePage";
 import Register from "./pages/register/Register";
 
 const App = () => {
+  const {user} = useContext(Context)
   return (
     <BrowserRouter>
       <Switch>
-        <Route path="/" exact component={Home} />
-        <Route path="/profile/:username" component={ProfilePage} />
-        <Route path="/login" exact component={Login} />
-        <Route path="/Register" exact component={Register} />
+        <Route path="/" exact component={user?Home : Login} />
+        <Route path="/profile/:username" component={user ? ProfilePage : Login} />
+        <Route path="/login" exact component={user?Home:Login} />
+        <Route path="/Register" exact component={user?Home:Register} />
       </Switch>
     </BrowserRouter>
   );
