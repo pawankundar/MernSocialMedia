ef29851865e25bbe3ea405780e1cc7d696d75ee2
pawankundar <pawankundar98@hotmail.com>
2021-09-27 18:22:38 +0530

    routing if no user found

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import { useContext } from "react";
-import { BrowserRouter, Switch, Route} from "react-router-dom";
+import { BrowserRouter, Switch, Route,Redirect} from "react-router-dom";
 import { Context } from "./context/context";
 import Home from "./pages/home/home";
 import Login from "./pages/login/Login";
@@ -12,9 +12,15 @@ const App = () => {
     <BrowserRouter>
       <Switch>
         <Route path="/" exact component={user?Home : Login} />
-        <Route path="/profile/:username" component={user ? ProfilePage : Login} />
-        <Route path="/login" exact component={user?Home:Login} />
-        <Route path="/Register" exact component={user?Home:Register} />
+        <Route path="/profile/:username"> 
+        {user ? <ProfilePage/> : <Redirect to="/"/>}
+        </Route>
+        <Route path="/login">
+          {user?<Redirect to="/"/>:<Login/>}
+        </Route>
+        <Route path="/Register">
+        {user?<Redirect to="/"/>:<Register/>}
+        </Route>
       </Switch>
     </BrowserRouter>
   );
