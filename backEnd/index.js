85710e998bf6e3827814299b26f02f3cf2915cdd
pawankundar <pawankundar98@hotmail.com>
2021-09-27 20:23:13 +0530

    upload image route added

--- backEnd/index.js
+++ backEnd/index.js
@@ -3,6 +3,8 @@ const App = express();
 const mongoose = require("mongoose");
 const helmet = require("helmet");
 const morgan = require("morgan");
+const multer = require('multer')
+const path = require('path')
 require("dotenv").config();
 const userRoute = require("./router/users");
 const authRoute = require("./router/auth");
@@ -22,6 +24,25 @@ mongoose
   })
   .then(console.log("database connected"))
   .catch((err) => console.log("error is ",err));
+
+App.use('/images',express.static(path.join(__dirname,"public/images")))
+
+//multer uploading images
+const storage = multer.diskStorage({
+  destination : (req,file,cb)=>{
+    cb(null ,"public/images/")
+  },
+  filename : (req,filename,cb)=>{
+    cb(null,req.body.name)
+  }
+})  
+const upload = multer({storage : storage})
+
+App.post('/api/upload',upload.single('file'),(req,res)=>{
+  res.status(200).json({
+    message : "file has been uploaded"
+  })
+})
 
 App.listen(process.env.PORT, () => {
   console.log("server running");
