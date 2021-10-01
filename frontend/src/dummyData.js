20e4a15094187edb88b131c79d370b4db07416d7
pawankundar <pawankundar98@hotmail.com>
2021-10-01 23:32:57 +0530

    placeholder data for online users

---
+++ frontend/src/dummyData.js
@@ -0,0 +1,20 @@
+export const Users = [
+  {
+
+    id:2,
+    profilePicture: "person/2.jpeg",
+    username: "Pawan Kundar",
+  },
+
+  {
+    id:5,
+    profilePicture: "person/5.jpeg",
+    username: "Nathan Drake",
+  },
+  {
+    id:8,
+    profilePicture: "person/1.jpg",
+    username: "Miles Morales",
+  },
+  
+];
