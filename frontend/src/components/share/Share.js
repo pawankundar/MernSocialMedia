f96c1d9e4f2377c985324b64d2d504a2b730975a
pawankundar <pawankundar98@hotmail.com>
2021-10-01 20:13:12 +0530

    preveiw for selected image

--- frontend/src/components/share/Share.js
+++ frontend/src/components/share/Share.js
@@ -1,10 +1,35 @@
-import { EmojiEmotions, Label, PermMedia, Room } from "@material-ui/icons";
-import { useContext } from "react";
+import { Cancel, EmojiEmotions, Label, PermMedia, Room } from "@material-ui/icons";
+import axios from "axios";
+import { useContext, useRef, useState } from "react";
 import { Context } from "../../context/context";
 import "./Share.css";
 const Share = () => {
   const {user} = useContext(Context)
   const ImageLink = process.env.REACT_APP_PUBLIC_FOLDER;
+  const [file,setFile] = useState(null)
+  const captionRef = useRef()
+
+  const handlePost = async(e)=>{
+    e.preventDefault()
+    const newPost = {
+      userId : user._id,
+      desc : captionRef.current.value
+    }
+    if(file){
+      const data = new FormData()
+      const fileName = "post/"+Date.now()+file.name
+      data.append("name",fileName)
+      data.append("file",file)
+      newPost.img = fileName
+      await axios.post('/upload',data)
+      .then(console.log("image uploaded"))
+      .catch(console.log("error in upload"))
+    }
+    await axios.post("/posts/create",newPost)
+    .then(window.location.reload())
+    .catch(err=>console.log(err))
+
+  }
   return (
     <div className="share">
       <div className="shareWrapper">
@@ -14,15 +39,21 @@ const Share = () => {
             src={user.profilePicture?ImageLink+user.profilePicture:ImageLink+"/person/no-avatar.png"}
             alt="share"
           />
-          <input className="shareInput" placeholder="whats on your mind" />
+          <input className="shareInput" placeholder={"whats on your mind "+user.username+" ?"} ref={captionRef}/>
         </div>
         <hr className="shareHr" />
+        {file &&
+         <div className="selectedImageContainer">
+           <img className = "selectedImage" src={URL.createObjectURL(file)} alt="selectedImage"/>
+           <Cancel className="cancelImage" onClick={() => setFile(null)}/>
+          </div>}
         <div className="shareBottom">
           <div className="shareOptions">
-            <div className="shareOption">
+            <label htmlFor="file" className="shareOption">
               <PermMedia htmlColor="tomato" className="shareIcon" />
               <span className="shareOptionText ">Photo or video</span>
-            </div>
+              <input style={{display : "none"}} type="file" id="file" accept=".png,.jpeg,.jpg" onChange={e=>setFile(e.target.files[0])}/>
+            </label>
             <div className="shareOption">
               <Label htmlColor="blue" className="shareIcon" />
               <span className="shareOptionText">Tag</span>
@@ -36,7 +67,7 @@ const Share = () => {
               <span className="shareOptionText">Feelings</span>
             </div>
           </div>
-          <button className="postButton">Post</button>
+          <button className="postButton" onClick={handlePost}>Post</button>
         </div>
       </div>
     </div>
