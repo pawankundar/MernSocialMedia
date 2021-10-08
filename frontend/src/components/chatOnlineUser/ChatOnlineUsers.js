b71abc484a4065433af17afc6f5c3295cbf7c4aa
pawankundar <pawankundar98@hotmail.com>
2021-10-08 22:57:52 +0530

    fetching online users from socket data

--- frontend/src/components/chatOnlineUser/ChatOnlineUsers.js
+++ frontend/src/components/chatOnlineUser/ChatOnlineUsers.js
@@ -1,19 +1,39 @@
-require('./ChatOnlineUser.css')
+import axios from "axios";
+import { useEffect, useState } from "react";
 
-const ChatOnlineUser = ()=>{
-    return(
-        <div className="chatOnline">
-             <span className="chatOnlineLabel">Online users.</span>
-             <hr className="chatOnlineHr"/>
-            <div className="chatOnlineFriend">
-                <div className="chatOnlineImgContainer">
-                    <img src="http://localhost:8000/images/person/1.jpg" className="chatOnlineImg" alt="onlineUserImage"/>
-                    <div src="chatOnlineBadge"></div>
-                </div>
-                <span className="chatOnlineName">Pawan Kundar</span>
-            </div>
+require("./ChatOnlineUser.css");
+
+const ChatOnlineUser = ({ userData, currentUser }) => {
+  const PF = process.env.REACT_APP_PUBLIC_FOLDER;
+
+  const [onlineUser, setOnlineUser] = useState([]);
+  useEffect(() => {
+    const getUser = async () => {
+      await axios
+        .get("/users/?userId=" + userData)
+        .then((resp) => setOnlineUser(resp.data));
+    };
+    getUser();
+  }, [userData]);
+  return (
+    <div className="chatOnline">
+      <div className="chatOnlineFriend">
+        <div className="chatOnlineImgContainer">
+          <img
+            src={
+              onlineUser?.profilePicture
+                ? PF + onlineUser.profilePicture
+                : PF + "person/no-avatar.png"
+            }
+            className="chatOnlineImg"
+            alt="onlineUserImage"
+          />
+          <div src="chatOnlineBadge"></div>
         </div>
-    )
-}
+        <span className="chatOnlineName">{onlineUser.username}</span>
+      </div>
+    </div>
+  );
+};
 
-export default ChatOnlineUser
+export default ChatOnlineUser;
