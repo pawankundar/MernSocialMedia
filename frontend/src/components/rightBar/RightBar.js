ffa86ff8a3089e1b0c842945a0b8612a23a0dad7
pawankundar <pawankundar98@hotmail.com>
2021-10-09 23:25:45 +0530

    removed the extra component

--- frontend/src/components/rightBar/RightBar.js
+++ frontend/src/components/rightBar/RightBar.js
@@ -1,60 +1,16 @@
 import "./RightBar.css";
 import { Users } from "../../dummyData";
 import OnlineUser from "../onlineUser/OnlineUser";
-import SideInfoFollowers from "../SideInfoFollowers/SideInfoFollowers";
-import { useContext, useEffect, useState } from "react";
-import axios from "axios";
-import { Context } from "../../context/context";
-import { Add } from "@material-ui/icons";
 
-const RightBar = ({ data }) => {
+
+const RightBar = () => {
 
   const photoUrl = process.env.REACT_APP_PUBLIC_FOLDER;
-  const { user: currentUser ,dispatch} = useContext(Context);
-  const relationshipStatus = ["Single", "Married", "Not intrested"];
-  const [followers, setFollowers] = useState([]);
-  const [followed,setFollowed] = useState(currentUser.following.includes(data?._id))
   
-
-
-  useEffect(() => {
-    const getFollowers = async () => {
-      axios
-        .get("/users/followers/" + data?._id)
-        .then((resp) => setFollowers(resp.data))
-        .catch(() => console.log("error in getFollowers RightBar"));
-    };
-    getFollowers();
-  }, [data?._id,followed]);
-
-  useEffect(()=>{
-    setFollowed(currentUser.following.includes(data?._id))
-  },
-  [data?._id,currentUser])
-
-  const handleFollow =async(e)=>{
-    e.preventDefault()
-    await axios.put(`/users/${data._id}/follow`,{
-      userId : currentUser._id
-    })
-    dispatch({type : "FOLLOW",payload : data._id})
-    setFollowed(!followed)
-  }
-
-  const handleUnfollow =  async(e)=>{
-    e.preventDefault()
-    await axios.put(`/users/${data._id}/unfollow`,{
-      userId : currentUser._id
-    })
-    dispatch({type : "UNFOLLOW",payload :data._id})
-    setFollowed(!followed)
-  }
-
-
-  const HomeRightBar = () => {
-    return (
-      <>
-        <div className="bdayContainer">
+  return (
+    <div className="rightBar">
+      <div className="rightBarWrapper">
+      <div className="bdayContainer">
           <img className="bdayImage" src={photoUrl+"gift.png"} alt="bdayPic" />
           <span className="bdayText">
             <b>Jhon doe</b> and <b>3 other friends</b> have a birthday
@@ -73,46 +29,6 @@ const RightBar = ({ data }) => {
             <OnlineUser data={onlineUser} key={onlineUser.id} />
           ))}
         </ul>
-      </>
-    );
-  };
-
-  const ProfileRightBar = () => {
-    return (
-      <>
-        {currentUser.username !== data.username &&
-         (<button  onClick={followed?handleUnfollow:handleFollow} className="followButton">{ followed?"Unfollow" :"Follow"} <Add/></button>)}
-        <h4 className="userInfoTitle">User information</h4>
-        <div className="rightBarInfo">
-          <div className="rightBarInfoItem">
-            <span className="rightBarInfoKey">City:</span>
-            <span className="rightBarInfoValue">{data.city}</span>
-          </div>
-          <div className="rightBarInfoItem">
-            <span className="rightBarInfoKey">From:</span>
-            <span className="rightBarInfoValue">{data.from}</span>
-          </div>
-          <div className="rightBarInfoItem">
-            <span className="rightBarInfoKey">Relationship:</span>
-            <span className="rightBarInfoValue">
-              {relationshipStatus[data.relationship - 1]}
-            </span>
-          </div>
-        </div>
-
-        <h4 className="userFriendsTitle">User followers</h4>
-        <div className="rightBarFollowings">
-          {followers?.map((follower) => (
-            <SideInfoFollowers follower={follower} />
-          ))}
-        </div>
-      </>
-    );
-  };
-  return (
-    <div className="rightBar">
-      <div className="rightBarWrapper">
-        {data ? <ProfileRightBar /> : <HomeRightBar />}
       </div>
     </div>
   );
